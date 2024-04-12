5b9701f32395266c809dd2809d318300d01df209
tommytt-ops <tommytt721@gmail.com>
2024-04-12 23:32:21 +0200

    no template

--- cluster_nodes/inventory.tpl
+++
@@ -1,4 +0,0 @@
-[kubernetes_cluster]
-{% for ip in ips.split(',') %}
-node{{ loop.index }} ansible_host={{ ip }} ansible_user=ubuntu
-{% endfor %}
--- cluster_nodes/node_server.tf
+++ cluster_nodes/node_server.tf
@@ -7,34 +7,34 @@ terraform {
 }
 
 provider "openstack" {
-  cloud = "openstack" 
+  cloud = "openstack"
 }
 
 resource "openstack_blockstorage_volume_v3" "boot_volume" {
-  count = 5
-  name  = "boot_volume${count.index+1}"
-  size  = 20
-  image_id = "6094568b-0d16-48a5-bc10-66645c361d4a"
+  count     = 5
+  name      = "boot_volume${count.index + 1}"
+  size      = 20
+  image_id  = "6094568b-0d16-48a5-bc10-66645c361d4a"
 }
 
 resource "openstack_blockstorage_volume_v3" "ceph_volume" {
   count = 5
-  name  = "ceph_volume${count.index+1}"
+  name  = "ceph_volume${count.index + 1}"
   size  = 20
 }
 
 resource "openstack_compute_instance_v2" "master_instance" {
-  count = 5
-  name          = "Node${count.index+1}"
-  flavor_name   = "css.2c6r.20g"
-  key_pair      = "Ansible"
-  security_groups = ["default"]
+  count            = 5
+  name             = "Node${count.index + 1}"
+  flavor_name      = "css.2c6r.20g"
+  key_pair         = "Ansible"
+  security_groups  = ["default"]
 
   block_device {
-    uuid                = openstack_blockstorage_volume_v3.boot_volume[count.index].id
-    source_type         = "volume"
-    destination_type    = "volume"
-    boot_index          = 0
+    uuid                  = openstack_blockstorage_volume_v3.boot_volume[count.index].id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = 0
     delete_on_termination = true
   }
 
@@ -51,28 +51,25 @@ resource "openstack_compute_instance_v2" "master_instance" {
 }
 
 resource "openstack_compute_volume_attach_v2" "ceph_volume_attach" {
-  count = 5
-  instance_id = openstack_compute_instance_v2.master_instance[count.index].id
-  volume_id   = openstack_blockstorage_volume_v3.ceph_volume[count.index].id
+  count        = 5
+  instance_id  = openstack_compute_instance_v2.master_instance[count.index].id
+  volume_id    = openstack_blockstorage_volume_v3.ceph_volume[count.index].id
 }
 
-data "template_file" "ansible_inventory" {
-  template = "${file("${path.module}/inventory.tpl")}"
-
-  vars = {
-    ips = join(",", [for instance in openstack_compute_instance_v2.master_instance : instance.access_ip_v4])
-  }
+locals {
+  inventory_content = <<EOT
+[kubernetes_cluster]
+%{ for idx, ip in openstack_compute_instance_v2.master_instance.*.access_ip_v4 ~}
+node${idx + 1} ansible_host=${ip} ansible_user=ubuntu
+%{ endfor ~}
+EOT
 }
 
 resource "local_file" "inventory" {
-  content  = data.template_file.ansible_inventory.rendered
+  content  = local.inventory_content
   filename = "${path.module}/ansible_inventory.ini"
 }
 
-output "debug_ips" {
-  value = join(",", [for instance in openstack_compute_instance_v2.master_instance : instance.access_ip_v4])
+output "inventory_content" {
+  value = local.inventory_content
 }
-
-
-
- 
