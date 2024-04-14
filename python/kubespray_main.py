62f7be2dbc3a3fdd75453ff9f8514487b17ec9f2
tommytt-ops <tommytt721@gmail.com>
2024-04-14 15:54:25 +0200

    terraform test

--- python/kubespray_main.py
+++ python/kubespray_main.py
@@ -1,5 +1,5 @@
 import os
-from kubespray_utils import run_setup_palybook, all_server_list
+from kubespray_utils import run_setup_palybook, all_server_list, apply_terraform
 
 
 if __name__ == "__main__":
@@ -8,6 +8,8 @@ if __name__ == "__main__":
 
     kubespray_dir="/home/ubuntu/kubespray"
     os.chdir(kubespray_dir)
+
+    apply_terraform()
 
     print(all_server_list())
 
