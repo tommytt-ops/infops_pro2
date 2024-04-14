5b28c7140e7509baed664620f2aaa8cbe3f6860b
tommytt-ops <tommytt721@gmail.com>
2024-04-14 18:09:30 +0200

    man

--- python/kubespray_utils.py
+++ python/kubespray_utils.py
@@ -65,10 +65,10 @@ def kubespray_run(ip_list):
         ("cp -rfp inventory/sample inventory/mycluster", "Copying inventory"),
         (f"CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py {ips_formatted}", "Running inventory builder"),
         ('echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ubuntu', "Setting sudoers for ubuntu"),
-        ("""ansible all -m shell -a "echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ubuntu" -i inventory/mycluster/hosts.yml --user ubuntu""", "Applying sudoers change via Ansible"),
-        ("""ansible all -m shell -a "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf" -i inventory/mycluster/hosts.yml --user ubuntu""", "Enabling IP forwarding"),
+        ("""ansible all -m shell -a "echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ubuntu" -i inventory/mycluster/hosts.yaml --user ubuntu""", "Applying sudoers change via Ansible"),
+        ("""ansible all -m shell -a "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf" -i inventory/mycluster/hosts.yaml --user ubuntu""", "Enabling IP forwarding"),
         ("""ansible all -m shell -a "sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && sudo swapoff -a" -i inventory/mycluster/hosts.yml --user ubuntu""", "Disabling swap and updating fstab"),
-        ("""ansible-playbook -i inventory/mycluster/hosts.yml --become --become-user=root cluster.yml --user ubuntu""", "kubespray successful")
+        ("""ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root cluster.yml --user ubuntu""", "kubespray successful")
     ]
 
     for command, description in commands:
